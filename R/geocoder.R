251e933e5294fbea8a751d0b778065126686cc82
bransonf <32143148+bransonf@users.noreply.github.com>
2019-05-15 21:46:41 -0500

    refactor and update vintage to current

--- R/geocoder.R
+++ R/geocoder.R
@@ -1,65 +1,33 @@
 # This is the internal function for geocoding
-# much of the code is borrowed from https://github.com/dmwelgus/MapChi
-#
-#
-census_geocoder <- function (df){
 
-  # convert a df to a csv in a temporary directory.
+census_geocoder <- function(.data){
+
+  # create and store a csv in a temp dir
   tmp <- tempdir()
-  readr::write_csv(df, path = paste0(tmp, "addresses.csv"), col_names = FALSE)
+  readr::write_csv(.data, path = paste0(tmp, "addresses.csv"), col_names = FALSE)
   file <- paste0(tmp, "addresses.csv")
 
-  # This part posts a file of addresses
+  # send file as request
   response <- httr::POST("https://geocoding.geo.census.gov/geocoder/locations/addressbatch",
-                    body = list(addressFile = httr::upload_file(file),
-                                benchmark = 9, vintage = "Census2010_Census2010"),
-                    encode = "multipart")
+                         body = list(addressFile = httr::upload_file(file),
+                                     benchmark = "Public_AR_Current", vintage = "Current_Current"),
+                         encode = "multipart")
 
-  ###### Functions need to be defined----------------------------------
-  length_15 <- function(x) {
-    if (length(x) == 15) {
-      x <- x
-    } else {
-      x <- c(x, rep(NA, 15-length(x)))
-    }
-    return(x)
-  }
-  strip_both <- function(x) {
-    n <- nchar(x)
-    return(substr(x, 2, n - 1))
-  }
-  strip_left <- function(x) {
-    n <- nchar(x)
-    return(substr(x, 2, n))
-  }
-  strip_right <- function(x) {
-    n <- nchar(x)
-    return(substr(x, 1, n - 1))
-  }
-  #####--------------------------------------------------
+  # parse as csv
+  text <- httr::content(response, as = "text", encoding = "UTF-8")
+  df <- read.csv(text = text, header = FALSE, stringsAsFactors = FALSE)
 
-  # begin parsing
-  content <- httr::content(response, encoding = "UTF-8")
-  df1 <- strsplit(content, split = "\\\n")[[1]]
-  df2 <- sapply(df1, strsplit, split = ",")
-  clean <- lapply(df2, length_15)
+  # split and coerce class of coords
+  df <- tidyr::separate(df, V6, c("lon", "lat"), sep = ",") %>%
+    dplyr::mutate(lon = as(lon, "numeric"),
+                  lat = as(lat, "numeric"))
 
-  ####
-  h1   <- t(as.data.frame(clean)) #transpose df
-  h2   <- as.data.frame(h1)
-  h2[] <- lapply(h2, as.character)
+  # split o_address into components
+  df <- tidyr::separate(df, V2, c("o_address", "o_city" ,"o_state", "o_zip"), sep = ",")
 
-  both_vars <- c("V1", "V5", "V6", "V7", "V11", "V14", "V15")
-  left_vars <- c("V2", "V8", "V12")
-  h2[, both_vars] <- lapply(h2[, both_vars], strip_both)
-  h2[, left_vars] <- lapply(h2[, left_vars], strip_left)
+  # apply names
+  names(df) <- c("id", "o_address", "o_city" ,"o_state", "o_zip", "status", "quality", "m_address", "lon", "lat", "TIGER_line_id", "side")
 
-  h2$V13 <- strip_right(h2$V13)
 
-  h2[] <- lapply(h2, stringr::str_trim)
-  names(h2) <- c("id", "o_address", "o_city", "o_state", "o_zip", "status", "quality",
-                "m_address", "m_city", "m_state", "m_zip", "long", "lat", "TIGER_line_id", "side")
-
-  return(data.frame(h2, row.names = NULL)) # return df with no row names
-
+  return(df)
 }
