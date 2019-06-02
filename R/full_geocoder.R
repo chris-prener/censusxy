328249d9104d621b48e2bdbe851770ff67ccdb06
bransonf <32143148+bransonf@users.noreply.github.com>
2019-06-02 15:45:29 -0500

    full single function

---
+++ R/full_geocoder.R
@@ -0,0 +1,41 @@
+#' Geocode Addresses Using the Census Bureau Geocoder
+#'
+#' @param .data Dataframe containing address data
+#' @param id Unique identifier for
+#' @param address
+#' @param city
+#' @param state
+#' @param zip
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cxy_geocode <- function(.data, id = NA, address, city = NA, state = NA, zip = NA, timeout = 30){
+
+  #TODO Add NSE
+
+  #TODO Add warnings
+    # omission of City, State, Zip makes lesss accurate and significantly slower
+
+  # prepare and split
+  censusxy:::census_prep(.data, id, address, city, state, zip) -> prep
+  censusxy:::census_split(prep) -> split
+
+  # convert timeout to min
+  timeout <- timeout * 60
+
+  response <- vector("list", length(split))
+
+  for (i in seq_along(split)) {
+   response[[i]] <- try(
+     censusxy:::census_geocoder(split[[i]], timeout)
+     )
+  }
+
+  response <- dplyr::bind_rows(response) # if any errors, this will trigger another error, need to remove class try catch
+  result <- left_join(prep, response, by = c("address", "city", "state", "zip"))
+
+  # return result
+  return(result)
+}
