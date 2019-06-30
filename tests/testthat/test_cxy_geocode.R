e396a4fad5345a62b5ff1b7d17f7763ddd9fe57e
Christopher Prener <chris.prener@slu.edu>
2019-06-30 15:32:13 -0500

    change data source

--- tests/testthat/test_cxy_geocode.R
+++ tests/testthat/test_cxy_geocode.R
@@ -2,17 +2,17 @@ context("test cxy_geocode function")
 
 # load data -------------------------------------------------
 
-data(stl_homicides_small, package = "censusxy")
+x <- stl_homicides_small
 
 # test NSE and SE --------------------------------------------
 test_that("non-standard evaluation works", {
   expect_silent(
-    cxy_geocode(stl_homicides_small, address = street_address, city = city, state = state, zip = postal_code)
+    cxy_geocode(x, address = street_address, city = city, state = state, zip = postal_code)
   )
 })
 test_that("standard evaluation works", {
   expect_silent(
-    cxy_geocode(stl_homicides_small, address = "street_address", city = "city", state = "state", zip = "postal_code")
+    cxy_geocode(x, address = "street_address", city = "city", state = "state", zip = "postal_code")
   )
 })
 
@@ -22,12 +22,12 @@ test_that("standard evaluation works", {
 test_that("misspecified functions return errors", {
   expect_error(cxy_geocode(address = street_address, city = city, state = state),
                "An existing data frame or tibble must be specified for '.data'.")
-  expect_error(cxy_geocode(stl_homicides_small, city = city, state = state),
+  expect_error(cxy_geocode(x, city = city, state = state),
                "A column containing address must be supplied for 'address'.")
-  expect_error(cxy_geocode(stl_homicides_small, address = street_address, city = city, zip = postal_code, state = state,
+  expect_error(cxy_geocode(x, address = street_address, city = city, zip = postal_code, state = state,
                            timeout = "ham"),
                "A numeric value must be specified for 'timeout'.")
-  expect_error(cxy_geocode(stl_homicides_small, address = street_address, city = city, state = state, zip = postal_code, output = "ham"),
+  expect_error(cxy_geocode(x, address = street_address, city = city, state = state, zip = postal_code, output = "ham"),
                "Please choose one of 'tibble' or 'sf' for 'output'.")
 })
 
@@ -35,13 +35,13 @@ test_that("misspecified functions return errors", {
 
 test_that("warning of ommission triggers for city, state, or zip", {
   expect_warning(
-    cxy_geocode(stl_homicides_small, address = street_address, city = city, state = state), "Omission of city, state or zip code greatly reduces the speed and accuracy of the geocoder"
+    cxy_geocode(x, address = street_address, city = city, state = state), "Omission of city, state or zip code greatly reduces the speed and accuracy of the geocoder"
   )
   expect_warning(
-    cxy_geocode(stl_homicides_small, address = street_address, city = city, zip = postal_code), "Omission of city, state or zip code greatly reduces the speed and accuracy of the geocoder"
+    cxy_geocode(x, address = street_address, city = city, zip = postal_code), "Omission of city, state or zip code greatly reduces the speed and accuracy of the geocoder"
   )
   expect_warning(
-    cxy_geocode(stl_homicides_small, address = street_address, state = state, zip = postal_code), "Omission of city, state or zip code greatly reduces the speed and accuracy of the geocoder"
+    cxy_geocode(x, address = street_address, state = state, zip = postal_code), "Omission of city, state or zip code greatly reduces the speed and accuracy of the geocoder"
   )
 
 })
@@ -51,10 +51,10 @@ test_that("warning of ommission triggers for city, state, or zip", {
 
 test_that("output type produces correct output class", {
   expect_s3_class(
-    cxy_geocode(stl_homicides_small, address = street_address, city = city, state = state, zip = postal_code, output = "sf"), "sf"
+    cxy_geocode(x, address = street_address, city = city, state = state, zip = postal_code, output = "sf"), "sf"
   )
   expect_s3_class(
-    cxy_geocode(stl_homicides_small, address = street_address, city = city, state = state, zip = postal_code, output = "tibble"), "tbl_df"
+    cxy_geocode(x, address = street_address, city = city, state = state, zip = postal_code, output = "tibble"), "tbl_df"
   )
 
 })
@@ -63,6 +63,6 @@ test_that("output type produces correct output class", {
 
 test_that("error is returned for zero matches", {
   expect_error(
-    suppressWarnings(cxy_geocode(stl_homicides_small, address = "street_address")), "No matches found for any of the supplied addresses. Make sure to include city, state and zip for best results"
+    suppressWarnings(cxy_geocode(x, address = "street_address")), "No matches found for any of the supplied addresses. Make sure to include city, state and zip for best results"
   )
 })
